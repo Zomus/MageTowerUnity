ff3cdc96bf81181ba8ec6e5fd657c46dbaaa0de9
Duncan Lo <duncanlo@MacBook-Air.local>
2017-01-25 17:16:23 -0700

    Mage Hand can now grab when player clicks. Mage hand will also begin walking when it is very close to the ground.

--- MageTower-2/Assets/MageHand/MageHandController.cs
+++ MageTower-2/Assets/MageHand/MageHandController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class MageHandController : MonoBehaviour {
 
 	Animator anim;
-	int grabHash = Animator.StringToHash("Grab");
+	//int grabHash = Animator.StringToHash("Grab");
 
 	float cooldown = 0;
 	//float timer = 0;
@@ -16,10 +16,10 @@ public class MageHandController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 3);
+		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -4);
 		//point of the mouse using screen coordinates
 
-		Vector3 offset = new Vector3(2f, -4.5f, 4.5f);
+		Vector3 offset = new Vector3(1.5f, -3.5f, 4.5f);
 		//offset between the coordinate offset of the (0, 0, 0) of the hand and its true center
 
 		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
@@ -28,17 +28,37 @@ public class MageHandController : MonoBehaviour {
 		gameObject.transform.position = curPosition + offset;
 		//change the gameObject's position to follow the mouse
 
-		if(Input.GetKeyDown(KeyCode.Space)){
-			anim.SetTrigger(grabHash);
-			cooldown = 1;
-		}
-		if(cooldown <= 0){
-			anim.ResetTrigger(grabHash);
-		}
-		else{
-			cooldown -= Time.deltaTime;
+		if(gameObject.transform.position.y < 0f){
+			gameObject.transform.Translate(new Vector3(0f, -gameObject.transform.position.y, 0f));
+			anim.SetBool("Walking", true);
+		}else{
+			anim.SetBool("Walking", false);
 		}
 
+
+		if(Input.GetMouseButtonDown(0)){
+			anim.SetBool("Grabbing", true);
+			cooldown = 1;
+		}
+		if(cooldown < 0){
+			anim.SetBool("Grabbing", false);
+		}
+
+		else{
+			cooldown -= Time.deltaTime * 2;
+		}
+		//Debug.Log("Grabbing: "+anim.GetBool("Grabbing"));
 	}
 	// Targets point when mouse left click is held.
 }
+
+/*
+if (Input.GetMouseButtonDown(0))
+	Debug.Log("Pressed left click.");
+
+if (Input.GetMouseButtonDown(1))
+	Debug.Log("Pressed right click.");
+
+if (Input.GetMouseButtonDown(2))
+	Debug.Log("Pressed middle click.");
+*/
