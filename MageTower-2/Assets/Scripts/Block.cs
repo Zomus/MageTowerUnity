d5becce4cd56645a532c472bc3ea3334dec116ed
Duncan Lo <duncanlo@MacBook-Air.local>
2017-04-07 19:55:42 -0600

    New tower model with blocks. Complete with textures and trap placing. Navmeshes baked and plane installed as an obstacle.

--- MageTower-2/Assets/Scripts/Block.cs
+++ MageTower-2/Assets/Scripts/Block.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
+
+using System;
 using System.Collections;
-
-
-
+using System.Collections.Generic;
 
 public class Block : MonoBehaviour {
+	public Material defaultMat;
+
 	//TRAP TYPES
 	public const int NO_TRAP = 0;
 	public const int SPRING_TRAP = 1;
@@ -16,17 +18,44 @@ public class Block : MonoBehaviour {
 	void Start () {
 		trapType = NO_TRAP;
 		//start by having no trap
-
-		assignNewMaterial("Dirt");
 		tag = "Trappable";
+		//allow the block to be trappable
+		defaultMat = getChild("Top").GetComponent<MeshRenderer>().material;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
-	public void assignNewMaterial(string name){
+	/*public void assignNewMaterial(string name){
 		Material newMat = Resources.Load("Materials/"+name, typeof(Material)) as Material;
 		gameObject.GetComponent<MeshRenderer>().material = newMat;
+	}*/
+	public void assignNewMaterial(string name){
+		if(name != "Default"){ //if parameter 'name' is not "Default"
+			Material newMat = Resources.Load("Materials/"+name, typeof(Material)) as Material;
+			//Look up newly assigned material in resources folder
+			getChild("Top").GetComponent<MeshRenderer>().material = newMat;
+			//assign new material
+		}else{
+			getChild("Top").GetComponent<MeshRenderer>().material = defaultMat;
+			//assign default material if parameter 'name' is "Default"
+		}
+	}
+
+	public Transform getChild(string name){
+		//getChild obtains the Transform of a child in this GameObject with the name 'name'
+
+		Transform selectedChild;
+		//Transform of the selected child
+
+		foreach (Transform child in transform){ //for loop searches through the parent GameObject's Transform
+			if (child.name == name){ //if names match
+				return child;
+				//return that the child's reference
+			}
+		}
+
+		Debug.Log("Error: Child cannot be found.");
+		return null;
 	}
 }
--- MageTower-2/Assets/Scripts/SceneController.cs
+++ MageTower-2/Assets/Scripts/SceneController.cs
@@ -2,93 +2,88 @@
 using System.Collections;
 
 public class SceneController : MonoBehaviour {
-
+	//Raycasting for mouse selection
 	Ray ray;
 	RaycastHit hit;
 
 	public GameObject tileContainer;
+	//Empty Object that contains all spawned tiles
 	public GameObject tilePrefab;
+	//Prefab (blueprint) of spawned tiles
 
 	public GameObject trapContainer;
+	//Empty Object that contains all spawned traps
 	public GameObject springTrapPrefab;
+	//Prefab (blueprint) of spawned springs
 
 	public Block highlightedTile;
 	//Currently selected tile
 
 	// Use this for initialization
 	void Start () {
+		//SPAWN ALL BLOCKS
 		int height = 0;
 		//represents height of blocks initialized currently
 		for(int i = -14; i < 14; i++){
 			for(int j = -1; j < 1; j++){
-				//Debug.Log(tileContainer+" "+i+" "+j);
-				//Create game tile and load it onto the screen
-				GameObject tempTile = Instantiate(tilePrefab, new Vector3(i + 0.5f, height, j + 0.5f), Quaternion.identity, tileContainer.transform) as GameObject;
-				tempTile.transform.localScale = new Vector3(0.8f, 0.5f, 0.8f);
-				tempTile.AddComponent<Block>();
-				tempTile.AddComponent<BoxCollider>();
-				//Add the Block script to let it function as a block
+				spawnTile(i, height, j);
 			}
 		}
 
 		for(int i = -2; i < 4; i++){
 			for(int j = 1; j < 3; j++){
-				//Debug.Log(tileContainer+" "+i+" "+j);
-				//Create game tile and load it onto the screen
-				GameObject tempTile = Instantiate(tilePrefab, new Vector3(i + 0.5f, height, j + 0.5f), Quaternion.identity, tileContainer.transform) as GameObject;
-				tempTile.transform.localScale = new Vector3(0.8f, 0.5f, 0.8f);
-				tempTile.AddComponent<Block>();
-				tempTile.AddComponent<BoxCollider>();
-				//Add the Block script to let it function as a block
+				spawnTile(i, height, j);
 			}
 		}
 
 		height = 5;
+		//new spawn height
 
 		for(int i = -2; i < 4; i++){
-			for(int j = 1; j < 4; j++){
-				//Debug.Log(tileContainer+" "+i+" "+j);
-				//Create game tile and load it onto the screen
+			for(int j = 1; j < 3; j++){
 				if(i == 2 && j == 2){
-					j++;
+					break;
 				}//traps cannot be placed on location where the player climbs up
-				GameObject tempTile = Instantiate(tilePrefab, new Vector3(i + 0.5f, height, j + 0.5f), Quaternion.identity, tileContainer.transform) as GameObject;
-				tempTile.transform.localScale = new Vector3(0.8f, 0.5f, 0.8f);
-				tempTile.AddComponent<Block>();
-				tempTile.AddComponent<BoxCollider>();
-				//Add the Block script to let it function as a block
+				spawnTile(i, height, j);
 			}
 		}
+		//FINISH SPAWNING BLOCKS
+	}
+
+	void spawnTile(float xPos, float yPos, float zPos){
+		//Creates a single game tile at (xPos, yPos, zPos) and load it onto the screen
+		GameObject tempTile = Instantiate(tilePrefab, new Vector3(xPos + 0.5f, yPos - 0.5f, zPos + 0.5f), Quaternion.identity, tileContainer.transform) as GameObject;
+		tempTile.transform.localScale = new Vector3(1f, 1f, 1f);
+		tempTile.AddComponent<Block>();
+		//Add the Block script to let it function as a block
+		tempTile.AddComponent<BoxCollider>();
+		//Add a collider to the tile to allow 
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if(highlightedTile != null){
-			highlightedTile.assignNewMaterial("Dirt");
+			highlightedTile.assignNewMaterial("Default");
 			highlightedTile = null;
-		}
-		placeTrap();
+		}//reassign default material to reset texture of selected tile every frame
+		castRay();
 	}
 
-	void assignNewMaterial(string name){
-		Material newMat = Resources.Load("Materials/"+name, typeof(Material)) as Material;
-		gameObject.GetComponent<MeshRenderer>().material = newMat;
-	}
-
-	void placeTrap(){
-		//Selection
+	void castRay(){
+		//Casts a ray to determine if any tile is selected and perform other actions accordingly
 		int layerMask = 1 << 8;
 		layerMask = ~layerMask;
+		//Creates a layermask that allows the ray to pass through the layer that contains the mage hand (as to not select the hand)
 
 		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		// casts a ray from camera to the point where your mouse is hovering over
+		//casts a ray from camera to the point where your mouse is hovering over
 
 		if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask)){ //return true if hits and return the hit object as a RaycastHit
-			if (hit.collider.tag == "Trappable"){
-				//Highlight block
+			if (hit.collider.tag == "Trappable"){ //If the object hit is marked as "Trappable" by a tag
 				highlightedTile = hit.collider.gameObject.GetComponent<Block>();
 				//select highlighted tile
-				hit.collider.gameObject.GetComponent<Block>().assignNewMaterial("Grass");
+				hit.collider.gameObject.GetComponent<Block>().assignNewMaterial("GrassTop");
+				//highlight the tile
 			}
 		}
 
@@ -96,11 +91,17 @@ public class SceneController : MonoBehaviour {
 		if (Input.GetMouseButtonUp(0)){ // on click
 			if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask)){ //return true if hits and return the hit object as a RaycastHit
 				if (hit.collider.tag == "Trappable" && hit.collider.gameObject.GetComponent<Block>().trapType == 0){ //Object is trappable and no trap has been placed on it
-					Debug.Log("hi");
-					//Place
+					Debug.Log("Trap placed");
+
+					//PLACE TRAP
 					hit.collider.gameObject.GetComponent<Block>().trapType = 1;
-					GameObject tempTrap = Instantiate(springTrapPrefab, hit.collider.gameObject.transform.position, Quaternion.identity, trapContainer.transform) as GameObject;
+					//Mark the trap type of the block to 1 to store that a trap has been placed
+					Vector3 dropLocation = hit.collider.gameObject.transform.position + new Vector3 (0f, 0.3f, 0f);
+					//Define where the trap is to be placed
+					GameObject tempTrap = Instantiate(springTrapPrefab, dropLocation, Quaternion.identity, trapContainer.transform) as GameObject;
+					//Spawn the trap
 					tempTrap.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+					//Scale the trap down
 				}
 			}
 		}
